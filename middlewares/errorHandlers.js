abb749da39b09515b64047502ce8a6b568aaad81
fikri marzuki <fikri.marzuki1993@gmail.com>
2020-12-18 11:02:55 +0700

    add error handlers

--- middlewares/errorHandlers.js
+++ middlewares/errorHandlers.js
@@ -1,5 +1,4 @@
 module.exports = function (err, req, res, next) {
-	console.log(err);
 	if (err && err.name && err.status) {
 		res.status(err.status).json({ error: err.name, msg: err.msg });
 	} else {
